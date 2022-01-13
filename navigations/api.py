920e5b6bcc56e6684dbf4cd5bcbc8d021225b855
Andrei Teleron <ateleron2@gmail.com>
2022-01-13 01:04:38 -0600

    Added way to query navigations api using pk or slug of the specific navigation item

--- navigations/api.py
+++ navigations/api.py
@@ -14,8 +14,47 @@ from wagtail.api.v2.serializers import BaseSerializer
 
 from .models import Navigation
 
+# @todo: figure out how to output api navigations to use slug rather than pk
+# See: https://wagtail.io/blog/wagtail-api-how-customize-detail-url/
+from django.core.exceptions import MultipleObjectsReturned
+from django.shortcuts import redirect
+from django.urls import reverse, path
 
 class NavigationsAPIViewset (BaseAPIViewSet):
-    model = Navigation
+  """
+  Defines the Navigations model API endpoints. The detail
+  views can now be queried using either the pk or the slug
+  of the specific Navigation object.
 
-    #body_fields = ['id', 'title',]
+  Thanks to: https://wagtail.io/blog/wagtail-api-how-customize-detail-url/
+  """
+  
+  model = Navigation
+  #body_fields = ['id', 'title', 'slug',]
+
+  def detail_view(self, request, pk=None, slug=None):
+    param = pk
+    if slug is not None:
+      self.lookup_field = 'slug'
+      param = slug
+    try:
+      return super().detail_view(request, param)
+    except MultipleObjectsReturned:
+      # Redirect to the listing view, filtered by the relevant slug
+      # The router is registered with the `wagtailapi` namespace,
+      # `pages` is our endpoint namespace and `listing` is the listing view url name.
+      return redirect(
+        reverse('wagtailapi:navigations:listing') + f'?{self.lookup_field}={param}'
+      )
+
+  @classmethod
+  def get_urlpatterns(cls):
+    """
+    This returns a list of URL patterns for the endpoint
+    """
+    return [
+      path('', cls.as_view({'get': 'listing_view'}), name='listing'),
+      path('<int:pk>/', cls.as_view({'get': 'detail_view'}), name='detail'),
+      path('<slug:slug>/', cls.as_view({'get': 'detail_view'}), name='detail'),
+      path('find/', cls.as_view({'get': 'find_view'}), name='find'),
+    ]
